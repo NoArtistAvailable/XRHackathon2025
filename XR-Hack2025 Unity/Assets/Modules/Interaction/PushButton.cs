17e7fd1ce0eda3b11a933d7458feaafaf9fce7a3
zach <felix.reinemann@gmail.com>
2025-03-12 08:20:25 +0100

    added push button

---
+++ XR-Hack2025 Unity/Assets/Modules/Interaction/PushButton.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PushButton : MonoBehaviour
+{
+    public UnityEvent OnPush;
+    public float pushedInThreshold = 0.01f;
+    private Rigidbody buttonBody;
+    private bool pushedIn = false;
+
+    private void OnEnable()
+    {
+        buttonBody = GetComponentInChildren<SpringJoint>()?.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        bool shouldBePushedIn = buttonBody.transform.localPosition.y <= pushedInThreshold;
+        if(!pushedIn && shouldBePushedIn) OnPush?.Invoke();
+        pushedIn = shouldBePushedIn;
+    }
+}
